2f78d798300a4a9a6c6470394182cfc06b15b3e7
blandashes <dawn1next@gmail.com>
2024-06-20 16:05:39 +0100

    changed how the padel script is called

--- regression-app2.py
+++ regression-app2.py
@@ -52,9 +52,10 @@ if selected == 'AChE Inhibitor Prediction model using pubchemfingerprints':
       
     def desc_calc():
         # Performs the descriptor calculation
-        bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
+        # bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+        # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+        subprocess.run("padel.sh", shell = True)
         os.remove('molecule.smi')
 
     
